0d9149448c68eac20b943f494a88656a62052a21
roszpapak <roszpapakrisztian@gmail.com>
2022-05-01 22:44:28 +0200

    Hazi JUNIT

--- src/test/java/coins/state/CoinsTest.java
+++ src/test/java/coins/state/CoinsTest.java
@@ -1,6 +1,7 @@
 package coins.state;
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
 
 import java.util.BitSet;
 
@@ -15,6 +16,42 @@ class CoinsTest {
         BitSet bs = new BitSet(7);
         bs.set(0, 7);
         state2 = new Coins(7, 3, bs);
+    }
+
+
+    @Test
+    void isGoalTest(){
+        assertEquals(true,state2.isGoal());
+        assertEquals(false, state1.isGoal());
+    }
+
+    @Test
+
+    void canFlipTest(){
+
+        assertEquals(false,state1.getCoins().length() <= state1.getN() && state1.getCoins().cardinality() == state1.getM());
+        assertEquals(false,state2.getCoins().length() <= state2.getN() && state2.getCoins().cardinality() == state2.getM());
+    }
+
+    @Test
+    void testClone() {
+        var clone = state1.clone();
+        assertTrue(clone.equals(state1));
+        assertNotSame(state1, clone);
+    }
+
+    @Test
+    void testToString(){
+        assertEquals("O|O|O|O|O|O|O",state1.toString());
+        assertEquals(true,"O|O|O|O|O|O|O".equals(state1.toString()));
+        assertEquals(false,"O|O|O|O|O|O|0|".equals(state1.toString()));
+    }
+
+    @Test
+    void testHashCode(){
+        Coins coin = new Coins(state1.getN(), state1.getM(),state1.getCoins());
+        assertTrue(state1.hashCode() == state1.hashCode());
+        assertTrue(state1.hashCode() == coin.hashCode());
     }
 
 }
